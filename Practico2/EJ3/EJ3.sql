93fd747d5d04060433f4721831db1085e92e41e1
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2022-04-17 10:00:32 -0300

    Ej3 pronto

--- Practico2/EJ3/EJ3.sql
+++ Practico2/EJ3/EJ3.sql
@@ -45,7 +45,58 @@ insert into Registro (fechaRegistro, idOrigen, idDestino, idTren, duracionViaje_
 -- 2.2. “La distancia total recorrida por cada tren, no puede ser inferior a la suma de las distancias de los
 -- tramos recorridos por dicho tren.”
 
+CREATE OR REPLACE TRIGGER VALIDAR_DISTANCIA_TOTAL
+BEFORE INSERT OR UPDATE ON Tren
+FOR EACH ROW
+DECLARE
+    DISTANCIARECORRIDA NUMERIC;
+BEGIN
+    SELECT SUM(Tramo.distanciaKM) INTO DISTANCIARECORRIDA
+    FROM Registro INNER JOIN Tramo
+        ON Registro.idOrigen = tramo.idterminalorigen
+        AND Registro.idDestino = tramo.idterminaldestino
+    WHERE Registro.idTren = :NEW.idTren;
+
+    IF :NEW.kmRecorridos < DISTANCIARECORRIDA THEN
+        RAISE_APPLICATION_ERROR(-20043, 'La distancia recorrida es inferior a la suma de los tramos realizados');
+    END IF;
+END;
+
 
 -- 2.3. “La velocidad crucero del tren no puede superar a su velocidad máxima.”
+
+CREATE OR REPLACE TRIGGER VALIDAR_VELOCIDADES
+BEFORE INSERT OR UPDATE ON Tren
+FOR EACH ROW
+BEGIN
+    IF :NEW.velocidadMaxima_kph < :NEW.velocidadCrucero_kph THEN
+        RAISE_APPLICATION_ERROR(-20044, 'La velocidad maxima es menor que la velocidad crucero');
+    END IF;
+END;
+
 -- 2.4. “A partir del 01/Ene/2010, los trenes que realicen viajes entre ciudades de distintos países, no pueden
 -- ser anteriores al año 2005 y no pueden tener más de 1.000.000 km recorridos.”
+CREATE OR REPLACE TRIGGER VALIDAR_DATOS_VIAJES_INTERNACIONALES
+BEFORE INSERT OR UPDATE ON Registro
+FOR EACH ROW
+DECLARE
+    DISTANCIARECORRIDA NUMERIC;
+    IDPAIS1 VARCHAR;
+    IDPAIS2 VARCHAR;
+BEGIN
+    IF :NEW.fechaRegistro > (to_date('1/1/2010','dd/mm/yyyy')) THEN
+        SELECT idPais INTO IDPAIS1 FROM Terminal WHERE Terminal.idTerminal = :NEW.idOrigen;
+        SELECT idPais INTO IDPAIS2 FROM Terminal WHERE Terminal.idTerminal = :NEW.idDestino;
+        IF IDPAIS1 != IDPAIS2 THEN
+            SELECT SUM(Tramo.distanciaKM) INTO DISTANCIARECORRIDA
+            FROM Registro INNER JOIN Tramo
+                ON Registro.idOrigen = tramo.idterminalorigen
+                AND Registro.idDestino = tramo.idterminaldestino
+            WHERE Registro.idTren = :NEW.idTren;
+
+            IF DISTANCIARECORRIDA > 1000000 THEN
+                RAISE_APPLICATION_ERROR(-20045, 'El tren dado ha recorrido mas de 1 millon de kms y no puede ser usado para viajes internacionales');
+            END IF;
+        END IF;
+    END IF;
+END;
